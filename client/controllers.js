f4f534af27ef457959e35a49d233ded884856a4a
mzwae <mrmonjaie@gmail.com>
2017-12-20 11:46:17 +0000

    added controllers

---
+++ client/controllers.js
@@ -0,0 +1,61 @@
+angular
+  .module('myApp')
+  .controller('loginController', function ($scope, $location, AuthService) {
+    $scope.login = function () {
+
+      //initial values
+      $scope.error = false;
+      $scope.disabled = true;
+
+      // call login from service
+      AuthService.login($scope.loginForm.username, $scope.loginForm.password)
+        //handle success
+        .then(function () {
+          $location.path('/');
+          $scope.disabled = false;
+          $scope.loginForm = {};
+        })
+        //handle error
+        .catch(function () {
+          $scope.error = true;
+          $scope.errorMessage = "Invalid username and/or password";
+          $scope.disabled = false;
+          $scope.loginForm = {};
+        });
+    };
+  })
+  .controller('logoutController', function ($scope, $location, AuthService) {
+    $scope.logout = function () {
+      // call logout from service
+      AuthService
+        .logout()
+        .then(function () {
+          $location.path('/login');
+        });
+    };
+  })
+
+  .controller('registerController', function ($scope, $location, AuthService) {
+    $scope.register = function () {
+      //initial values
+      $scope.error = false;
+      $scope.disabled = true;
+
+      //call register from service
+      AuthService
+        .register($scope.registerForm.username, $scope.registerForm.password)
+        //handle success
+        .then(function () {
+          $location.path('/');
+          $scope.disabled = false;
+          $scope.registerForm = {};
+        })
+        //handle error
+        .catch(function () {
+          $scope.error = true;
+          $scope.errorMessage = "Oops! Something went wrong.";
+          $scope.disabled = false;
+          $scope.registerForm = {};
+        });
+    };
+  });
